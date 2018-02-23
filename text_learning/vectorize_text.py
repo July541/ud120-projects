d1dccd557a7e6ef41e29862c758903b8b6be6e94
July541 <zhulei1995@126.com>
2018-02-24 00:52:19 +0800

    finish text_learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,20 +41,25 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            traget_words = ["sara", "shackleton", "chris", "germani"]
+            for item in traget_words:
+                words = words.replace(item, '')
 
             ### append the text to word_data
+            word_data.append(words)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            from_data.append(0 if name == 'sara' else 1)
 
 
             email.close()
@@ -62,6 +67,8 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 print "emails processed"
 from_sara.close()
 from_chris.close()
+
+print "word data[152]: " + word_data[152] # clear signatures
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
@@ -71,5 +78,10 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+transformer = TfidfVectorizer(stop_words="english")
+tfidf = transformer.fit_transform(word_data)
+print len(transformer.get_feature_names())
+print transformer.get_feature_names()[34597]
